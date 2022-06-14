6bd2ec404514531e51c47211c7a42c468787462b
clakg <clakg@icloud.com>
2022-06-14 14:00:00 +0200

    design card and truncate description

--- mycellio/src/components/VideoListItem.vue
+++ mycellio/src/components/VideoListItem.vue
@@ -1,9 +1,9 @@
 <template>
-    <v-card hover :to="{ name: 'video-watch', params: { id: video.id } }" class="text-decoration-none mx-2 mb-10" max-width="400" height="600">
-        <v-img :src="video.attributes.thumbnail" height="350px"/>
-        <div class="pa-10">
+    <v-card hover :to="{ name: 'video-watch', params: { id: video.id } }" class="text-decoration-none mx-2 mb-10" max-width="400" height="450">
+        <v-img :src="video.attributes.thumbnail" height="240px"/>
+        <div class="pa-5">
             <h3 class="black--text">{{ video.attributes.name }}</h3>
-            <div class="text--secondary" v-html="video.attributes.description"></div>
+            <div class="text--secondary my-5">{{ video.attributes.description | truncate(100) }}</div>
             <span v-for="tag_id in video.attributes.tag_ids" :key="tag_id">
                 <router-link :to="{ name:'tag', params: {id: tag_id} }" class="text-decoration-none">
                     <v-btn dark rounded small class="tag-button mr-2">{{ getTag(tag_id).attributes.name }}</v-btn>
@@ -20,8 +20,15 @@ export default {
     computed: {
         ...mapGetters(["getTag"])
     },
-    props: ["video"]
-}
+    props: ["video"],
+    filters: {
+        truncate: function(data,num){
+            const descriptionTruncate = data.split("").slice(0, num).join("");
+            return descriptionTruncate + '...';
+        }
+    }
+    }
+
 </script>
 
 <style lang="scss">
